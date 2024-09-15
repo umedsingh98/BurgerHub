30472b9af49370550f6716785050548b2714cc0b
umedsingh98 <umedsingh0898@gmail.com>
2024-09-15 23:36:28 +0530

    font size

--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from "react";
+import React,{ useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../../styles/headerStyle.css";
