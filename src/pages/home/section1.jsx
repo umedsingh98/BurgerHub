ebcdf2586075b26220bad9403c46750d1167810b
umedsingh98 <umedsingh0898@gmail.com>
2024-09-15 23:21:56 +0530

    bg image

--- src/pages/home/section1.jsx
+++ src/pages/home/section1.jsx
@@ -12,9 +12,7 @@ function Section1() {
         <h1>Sink Your Teeth Into Perfection</h1>
         <h2 >Juicy, Handcrafted Burgers, Grilled to Perfection.</h2>
         <p className="pt-2 pb-2">
-          Enjoy our new Indian Tadka Masala Burger with Onion, Capsicum, Red
-          <br />
-          Chilli Sause and our secret ingredients.
+          Enjoy our new Indian Tadka Masala Burger with Onion, Capsicum, Red Chilli Sause and our secret ingredients.
         </p>
         <Link to="/order" className="btn btn-lg">
           <button className="order_now">Order Now</button>
